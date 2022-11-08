2d9271a5867a80891c85920a634d78931567e603
DmitryKorzhIT <DmitryKorzhIT@gmail.com>
2022-11-08 08:47:16 +0200

    Added simple animation.

--- code/player.py
+++ code/player.py
@@ -12,12 +12,20 @@ class Player(pygame.sprite.Sprite):
         image_height = self.image.get_height()
         self.image = pygame.transform.scale(self.image,
             (image_width * PLAYER_MULTIPLIER, image_height * PLAYER_MULTIPLIER))
-        self.rect = self.image.get_rect(topleft=(pos))
+        self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0,-6)
+
+        # graphics setup
+        self.import_player_assets()
+        self.status = 'down'
+
+        # animation
+        self.frame_index = 0
+        self.animation_speed = 0.08
 
         # movement
         self.direction = pygame.math.Vector2()
-        self.speed = 4
+        self.speed = 2
         self.obstacle_sprites = obstacle_sprites
 
         self.import_player_assets()
@@ -31,7 +39,18 @@ class Player(pygame.sprite.Sprite):
                            'down_sleep': []
                            }
         for animation_key, animation_val in self.animations.items():
-            self.animations[animation_key] = import_folder(f'{path}{animation_key}/')
+            animations = import_folder(f'{path}{animation_key}/')
+            animations_scaled = []
+
+            # scale each image in self.animations
+            for image in animations:
+                image_w = image.get_width()
+                image_h = image.get_height()
+                image_scaled = pygame.transform.scale(image,
+                    (image_w*PLAYER_MULTIPLIER,image_h*PLAYER_MULTIPLIER))
+                animations_scaled.append(image_scaled)
+
+            self.animations[animation_key] = animations_scaled
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -39,15 +58,19 @@ class Player(pygame.sprite.Sprite):
         # movement input
         if keys[pygame.K_w]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_s]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_a]:
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_d]:
             self.direction.x = 1
+            self.status = 'right'
         else:
             self.direction.x = 0
 
@@ -77,6 +100,18 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.x < 0:  # moving right
                         self.hitbox.left = sprite.hitbox.right
 
+    def animate(self):
+        animation = self.animations[self.status]
+
+        self.frame_index += self.animation_speed
+
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
+
     def update(self):
         self.input()
+        self.animate()
         self.move()
