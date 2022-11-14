f105f8963d6b8ac9586fd550af5eb670f3e91890
Dmitry Korzh <DmitryKorzhIT@gmail.com>
2022-11-14 09:02:51 +0200

    Made cleaner a ui.py file.

--- code/ui.py
+++ code/ui.py
@@ -4,14 +4,19 @@ class UI:
     def __init__(self):
         self.display_surf = pygame.display.get_surface()
 
-    def inventory_blocks(self,pos):
-        self.image = pygame.image.load('../graphics/ui/inventory_block.png').convert_alpha()
-        self.image_scaled = pygame.transform.scale(self.image,
-            ((self.image.get_height() * 2),(self.image.get_width() * 2)))
-        self.rect = self.image.get_rect(topleft = (pos))
+        # load data
+        self.inventory_box_image = \
+            pygame.image.load('../graphics/ui/inventory_block_1.png').convert_alpha()
 
-        self.display_surf.blit(self.image_scaled,self.rect)
+    def inventory_box(self,pos):
+        height = self.inventory_box_image.get_height() * 2
+        width = self.inventory_box_image.get_width() * 2
+        image_scaled = pygame.transform.scale(self.inventory_box_image,(height,width))
+        rect = image_scaled.get_rect(topleft = (pos))
+
+        self.display_surf.blit(image_scaled,rect)
 
     def display(self,player):
         for i in range(6):
-            self.inventory_blocks((539+(i*34),670))
+            pos = (539+(i*34),670)
+            self.inventory_box(pos)
